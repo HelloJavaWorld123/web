2312d09db5248a30a090da93ca56e2460c25593a
haoxb <haoxb@healthmall.cn>
2017-10-25 13:43:40 +0800

    个人账户校验

--- app/scripts/controllers/Saas/dividedConfig.js
+++ app/scripts/controllers/Saas/dividedConfig.js
@@ -6,12 +6,12 @@
  */
 //分成方设置
 'use strict';
-
 App.controller('dividedConfigController', ['$scope', '$stateParams', '$rootScope', '$http', 'ngProgressFactory', '$uibModal', 'toastr', function ($scope, $stateParams, $rootScope, $http, ngProgressFactory, $uibModal, toastr) {
-    $rootScope.comData={};
-    $rootScope.comData.uid="5536b420ad8c11e7b9def48e38c3c5b0";
-    $scope.data={};
-    $scope.gymRoleData={};
+    $rootScope.comData = {};
+
+    $rootScope.comData.uid = "5536b420ad8c11e7b9def48e38c3c5b0";
+    $scope.data = {};
+    $scope.gymRoleData = {};
     //分页
     $scope.PageIndex = $rootScope.PAGINATION_CONFIG.PAGEINDEX;
     $scope.PageSize = $rootScope.PAGINATION_CONFIG.PAGESIZE;
@@ -43,6 +43,7 @@ App.controller('dividedConfigController', ['$scope', '$stateParams', '$rootScope
         }).then(function (res) {
             if (res.data.code == 2000) {
                 $scope.data = res.data.data;
+                /*用于调试个人账户*/
             } else {
                 toastr.error(res.data.msg);
             }
@@ -50,7 +51,7 @@ App.controller('dividedConfigController', ['$scope', '$stateParams', '$rootScope
             console.log("失败状态码：" + rej.code, +",失败信息：" + rej.data);
         });
 
-    $http({
+        $http({
             url: $rootScope.api.getGymInfo,
             method: 'post',
             headers: {
@@ -69,14 +70,14 @@ App.controller('dividedConfigController', ['$scope', '$stateParams', '$rootScope
         });
     };
     $scope.query();
-    
+
     /*账户校验*/
-    $scope.doVerify=function (bankType,item) {
-        var params={
-            "bankCheckStatus":1,
-            "id":item.id,
+    $scope.doVerify = function (bankType, item) {
+        var params = {
+            "bankCheckStatus": 1,
+            "id": item.id,
         };
-        if(bankType.code==0){//公司账户
+        if (bankType.code == 0) {//公司账户
             $http({
                 url: $rootScope.api.companyVerify,
                 method: 'post',
@@ -86,43 +87,60 @@ App.controller('dividedConfigController', ['$scope', '$stateParams', '$rootScope
                 data: params,
             }).then(function (res) {
                 if (res.data.code == 2000) {
-                 toastr.success("我们会给该账户汇入一笔非常小的金额，请在收到后，登录平台进行验证");
+                    toastr.success("我们会给该账户汇入一笔非常小的金额，请在收到后，登录平台进行验证");
                     //变成校验中
-                   var timer=setTimeout(function () {
-                       $scope.data.bankCheckStatus.code=1;
-                   },700);
+                    var timer = setTimeout(function () {
+                        $scope.data.bankCheckStatus.code = 1;
+                    }, 700);
                 } else {
                     toastr.error(res.data.msg);
                 }
             }, function (rej) {
                 console.log("失败状态码：" + rej.code, +",失败信息：" + rej.data);
             });
-        }else{//个人账户
-
+        } else {//个人账户
+            $uibModal.open({
+                templateUrl: 'personVerifyAccountMoney.html',
+                controller: 'personVerifyController',
+                size: 'md',
+                resolve: {
+                    item: function () {
+                        return item;
+                    }
+                }
+            }).result.then(function () {
+                //close
+                $scope.query();
+            }, function () {
+                //dismiss
+            });
         }
     }
-    
+
     /*确认打款金额*/
-    $scope.doVerifyAccountMoney=function (data) {
+    $scope.doVerifyAccountMoney = function (data) {
+        var item;
         $uibModal.open({
-            templateUrl:'doVerifyAccountMoney.html',
-            controller:'doVerifyAccountMoneyController',
-            size:'md',
-            resolve:{
-                item:function () {
+            templateUrl: 'doVerifyAccountMoney.html',
+            controller: 'doVerifyAccountMoneyController',
+            size: 'md',
+            resolve: {
+                item: function () {
                     return data;
                 }
             }
         }).result.then(function () {
+            //close
             $scope.query();
-        },function () {
+        }, function () {
+            //dismiss
             $scope.query();
         });
-        
+
     }
 }]);
-
-App.controller('doVerifyAccountMoneyController', ['$scope', 'item','$stateParams', '$rootScope', '$http', 'ngProgressFactory', '$uibModal','$uibModalInstance', 'toastr', function ($scope, item,$stateParams, $rootScope, $http, ngProgressFactory, $uibModal, $uibModalInstance,toastr) {
+//公司账户
+App.controller('doVerifyAccountMoneyController', ['$scope', 'item', '$stateParams', '$rootScope', '$http', 'ngProgressFactory', '$uibModal', '$uibModalInstance', 'toastr', function ($scope, item, $stateParams, $rootScope, $http, ngProgressFactory, $uibModal, $uibModalInstance, toastr) {
     //查询账户信息+名下场馆
     $scope.query = function () {
         var params = {
@@ -191,6 +209,70 @@ App.controller('doVerifyAccountMoneyController', ['$scope', 'item','$stateParams
     };
     $scope.close = function () {
         $uibModalInstance.dismiss('close');
+    };
+
+}]);
+//个人账户
+App.controller('personVerifyController', ['$scope', 'item', '$stateParams', '$rootScope', '$http', 'ngProgressFactory', '$uibModal', '$uibModalInstance', 'toastr', function ($scope, item, $stateParams, $rootScope, $http, ngProgressFactory, $uibModal, $uibModalInstance, toastr) {
+    $scope.data={};
+    //查询账户信息+名下场馆
+    $scope.query = function () {
+        var params = {
+            "id": $rootScope.comData.uid,
+        };
+        $http({
+            url: $rootScope.api.getBankInfo,
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: params,
+        }).then(function (res) {
+            if (res.data.code == 2000) {
+                $rootScope.data = res.data.data;
+            } else {
+                toastr.error(res.data.msg);
+            }
+        }, function (rej) {
+            console.log("失败状态码：" + rej.code, +",失败信息：" + rej.data);
+        });
+    };
+    /*账户校验*/
+    $scope.data.bankCardNum == null ? $scope.data.bankCardNum=item.bankAccount : $scope.data.bankCardNum;
+    $scope.data.bankName == null ? $scope.data.bankName=item.bankName : $scope.data.bankName;
+    $scope.save = function () {
+
+        var params = {
+            "id": item.id,
+            "bankCardNum": $scope.data.bankCardNum == null ? item.bankAccount : $scope.data.bankCardNum,
+            "bankName": $scope.data.bankName == null ? item.bankName : $scope.data.bankName,
+            "idCardName": $scope.data.idCardName,
+            "idCard": $scope.data.idCard,
+            "mobile": $scope.data.mobile,
+
+        };
+        $http({
+            url: $rootScope.api.personVerify,
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: params
+        }).then(function (res) {
+            if (res.data.code == 2000) {
+                $scope.data = res.data.data;
+                $uibModalInstance.close(res.data.data);
+                toastr.success("校验通过");
+                $scope.query();
+            } else {
+                toastr.error(res.data.msg);
+            }
+        }, function (rej) {
+            console.log("失败状态码：" + rej.code, +",失败信息：" + rej.data);
+        });
+    };
+    $scope.close = function (dataCode) {
+        $uibModalInstance.close(dataCode);
     };
 
 }]);
